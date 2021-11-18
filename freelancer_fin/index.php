fb0fe02a467433860aaaf5e7108dd525ce22505b
Edgar Narvaez <88750371+NarvaezE@users.noreply.github.com>
2021-11-18 16:44:20 -0700

    Add files via upload

---
+++ freelancer_fin/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <?php include_once('./templates/head.php'); ?>
+    <title>Diseñador Freelancer</title>
+</head>
+<body>
+
+    <?php include_once './templates/header.php'; ?>
+
+    <?php include_once './templates/main.php'; ?>
+    
+    <?php include_once './templates/footer.php'; ?>
+</body>
+</html>
---
+++ freelancer_fin/templates/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+        <p>Todos los derechos reservados. Juan De la torre Freelancer</p>
+</footer>
